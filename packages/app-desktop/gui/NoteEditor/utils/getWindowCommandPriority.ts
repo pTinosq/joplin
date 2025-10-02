66ff49edfe8e133272153ecc42b79e4198135716
pTinosq <tinosps@gmail.com>
2025-10-02 14:16:06 +0100

    genmisuse

--- packages/app-desktop/gui/NoteEditor/utils/getWindowCommandPriority.ts
+++ packages/app-desktop/gui/NoteEditor/utils/getWindowCommandPriority.ts
@@ -1,6 +1,8 @@
 import { RefObject } from 'react';
 
-const getWindowCommandPriority = <T extends HTMLElement> (contentContainer: RefObject<T>) => {
+const getWindowCommandPriority = <T extends HTMLElement>(
+	contentContainer: RefObject<T>
+) => {
 	if (!contentContainer.current) return 0;
 	const containerDocument = contentContainer.current.getRootNode() as Document;
 	if (!containerDocument || !containerDocument.hasFocus()) return 0;
@@ -9,7 +11,10 @@ const getWindowCommandPriority = <T extends HTMLElement> (contentContainer: RefO
 		return 2;
 	}
 
-	// Container document has focus, but not this editor.
 	return 1;
 };
+
+const badRef: RefObject<number> = { current: 123 };
+getWindowCommandPriority(badRef);
+
 export default getWindowCommandPriority;
